0548f72741ba19eceab9870650100bbfbaf4c362
TerroR75 <kamilsulimierski@gmail.com>
2023-03-16 15:07:59 +0100

    Example API fetch call to get all users from database

--- src/components/ApplSent.tsx
+++ src/components/ApplSent.tsx
@@ -1,7 +1,14 @@
 import { Box, Paper } from '@mui/material';
 import Summary from './Summary';
 import { classes } from '../styles/classes';
+import { useEffect } from 'react';
+
 function ApplSent() {
+  useEffect(() => {
+    fetch('api/users/all')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <Box sx={[classes.gridColTwo, classes.fullRelatSize, { p: 3 }]}>
       <Summary />
